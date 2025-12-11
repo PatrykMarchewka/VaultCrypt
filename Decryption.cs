373bed7b5755df4b2b6e8180cd7b3cff4bd9ebaf
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-11 19:53:14 +0100

    Added Decryption.cs file
    - Added Decryption.cs file that will hold decryption methods
    - Added AesGcmDecryption class with method to decrypt AesGcm encryption

---
+++ Decryption.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultCrypt
+{
+    internal class Decryption
+    {
+
+        internal static class AesGcmDecryption
+        {
+            internal static byte[] DecryptBytes(byte[] data, Encryption.EncryptionProtocol encryption)
+            {
+                Span<byte> iv = data.AsSpan(0, 12);
+                Span<byte> tag = data.AsSpan(12, 16);
+                Span<byte> encryptedData = data.AsSpan(28);
+
+                byte[] decrypted = new byte[encryptedData.Length];
+                using (AesGcm aesGcm = new AesGcm(VaultSession.KEY, AesGcm.TagByteSizes.MaxSize))
+                {
+                    aesGcm.Decrypt(iv, encryptedData, tag, decrypted);
+                }
+
+                return decrypted;
+            }
+
+
+
+
+        }
+    }
+}
