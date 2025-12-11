9144f6cca716a2259db2b431c1802e698f9372cc
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-11 19:54:36 +0100

    Added Encryption.cs file
    - Added Encryption.cs file that will hold encryption methods
    - Added AesGcmEncryption class with method to encrypt with AesGcm

---
+++ Encryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Printing.IndexedProperties;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace VaultCrypt
+{
+    internal class Encryption
+    {
+    internal static class AesGcmEncryption
+    {
+        internal static byte[] EncryptBytes(byte[] data, byte[] key)
+        {
+            using (AesGcm aesGcm = new AesGcm(key, AesGcm.TagByteSizes.MaxSize))
+            {
+                byte[] iv = new byte[AesGcm.NonceByteSizes.MaxSize];
+                RandomNumberGenerator.Fill(iv);
+                byte[] authentication = new byte[AesGcm.TagByteSizes.MaxSize];
+                byte[] output = new byte[data.Length];
+
+                aesGcm.Encrypt(iv, data, output, authentication);
+
+                byte[] encrypted = new byte[iv.Length + authentication.Length + output.Length];
+
+
+                Buffer.BlockCopy(iv, 0, encrypted, 0, iv.Length);
+                Buffer.BlockCopy(authentication, 0, encrypted, iv.Length, authentication.Length);
+                Buffer.BlockCopy(output, 0, encrypted, iv.Length + authentication.Length, output.Length);
+                return encrypted;
+            }
+        }
+
+
+        
+
+
+
+
+
+
+    }
+}
