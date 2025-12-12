7bc4b0b09b8c7359da88870ca86d4314010ff695
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-12 19:46:23 +0100

    Updated Decryption.cs
    - Modified AesGcmDecryption.DecryptBytes to take data as ReadOnlySpan<byte> instead of byte[]

--- Decryption.cs
+++ Decryption.cs
@@ -9,14 +9,13 @@ namespace VaultCrypt
 {
     internal class Decryption
     {
-
         internal static class AesGcmDecryption
         {
-            internal static byte[] DecryptBytes(byte[] data, Encryption.EncryptionProtocol encryption)
+            internal static byte[] DecryptBytes(ReadOnlySpan<byte> data, EncryptionOptions.EncryptionProtocol encryption = EncryptionOptions.EncryptionProtocol.AES256GCM)
             {
-                Span<byte> iv = data.AsSpan(0, 12);
-                Span<byte> tag = data.AsSpan(12, 16);
-                Span<byte> encryptedData = data.AsSpan(28);
+                ReadOnlySpan<byte> iv = data.Slice(0,12);
+                ReadOnlySpan<byte> tag = data.Slice(12, 16);
+                ReadOnlySpan<byte> encryptedData = data.Slice(28);
 
                 byte[] decrypted = new byte[encryptedData.Length];
                 using (AesGcm aesGcm = new AesGcm(VaultSession.KEY, AesGcm.TagByteSizes.MaxSize))
@@ -26,10 +25,6 @@ namespace VaultCrypt
 
                 return decrypted;
             }
-
-
-
-
         }
     }
 }
