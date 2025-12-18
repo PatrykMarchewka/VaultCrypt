fb4ccd005a11568254d4eda00dbe02cd2d3d971d
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-19 00:29:19 +0100

    Fix formatting
    - Fixed formatting in AesGcmEcncryption class

--- Encryption.cs
+++ Encryption.cs
@@ -16,26 +16,28 @@ namespace VaultCrypt
 {
     internal class Encryption
     {
-    internal static class AesGcmEncryption
-    {
-        internal static byte[] EncryptBytes(byte[] data, byte[] key)
         {
-                using (AesGcm aesGcm = new AesGcm(key, 16))
+        internal static class AesGcmEncryption
+        {
+            internal static byte[] EncryptBytes(byte[] data, byte[] key)
             {
+                using (AesGcm aesGcm = new AesGcm(key, 16))
+                {
                     byte[] iv = new byte[12];
-                RandomNumberGenerator.Fill(iv);
+                    RandomNumberGenerator.Fill(iv);
                     byte[] authentication = new byte[16];
-                byte[] output = new byte[data.Length];
+                    byte[] output = new byte[data.Length];
 
-                aesGcm.Encrypt(iv, data, output, authentication);
+                    aesGcm.Encrypt(iv, data, output, authentication);
 
-                byte[] encrypted = new byte[iv.Length + authentication.Length + output.Length];
+                    byte[] encrypted = new byte[iv.Length + authentication.Length + output.Length];
 
 
-                Buffer.BlockCopy(iv, 0, encrypted, 0, iv.Length);
-                Buffer.BlockCopy(authentication, 0, encrypted, iv.Length, authentication.Length);
-                Buffer.BlockCopy(output, 0, encrypted, iv.Length + authentication.Length, output.Length);
-                return encrypted;
+                    Buffer.BlockCopy(iv, 0, encrypted, 0, iv.Length);
+                    Buffer.BlockCopy(authentication, 0, encrypted, iv.Length, authentication.Length);
+                    Buffer.BlockCopy(output, 0, encrypted, iv.Length + authentication.Length, output.Length);
+                    return encrypted;
+                }
             }
         }
 
@@ -47,6 +49,7 @@ namespace VaultCrypt
 
 
 
+
+        }
     }
-}
 }
