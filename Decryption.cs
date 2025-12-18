a997fcdd4c740b27a76c51b026db0839cecacda3
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-19 00:08:48 +0100

    Update DecryptBytes method
    - Updated DecryptBytes method signature to no longer require EncryptionProtocol as all AESGCM encrypted data will be decrypted in the same way
    - Updated DecryptBytes to hardcode TagSize instead of using provided MaxSize field to prevent issues if Microsoft changes it

--- Decryption.cs
+++ Decryption.cs
@@ -11,14 +11,14 @@ namespace VaultCrypt
     {
         internal static class AesGcmDecryption
         {
-            internal static byte[] DecryptBytes(ReadOnlySpan<byte> data, EncryptionOptions.EncryptionProtocol encryption = EncryptionOptions.EncryptionProtocol.AES256GCM)
+            internal static byte[] DecryptBytes(ReadOnlySpan<byte> data, byte[] key)
             {
                 ReadOnlySpan<byte> iv = data.Slice(0,12);
                 ReadOnlySpan<byte> tag = data.Slice(12, 16);
                 ReadOnlySpan<byte> encryptedData = data.Slice(28);
 
                 byte[] decrypted = new byte[encryptedData.Length];
-                using (AesGcm aesGcm = new AesGcm(VaultSession.KEY, AesGcm.TagByteSizes.MaxSize))
+                using (AesGcm aesGcm = new AesGcm(key, 16))
                 {
                     aesGcm.Decrypt(iv, encryptedData, tag, decrypted);
                 }
