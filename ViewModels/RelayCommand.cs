390c029373b0fd7ac04ad1163e14d3e85ed0b35b
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-30 20:21:09 +0100

    Add RelayCommand class implementing ICommand
    - Added RelayCommand class that implements ICommand

---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VaultCrypt.ViewModels
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        internal RelayCommand(Action<object> execute, Func<object,bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        internal RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+
+        public bool CanExecute(object parameter) { return _canExecute?.Invoke(parameter) ?? true; }
+        public void Execute(object parameter) { _execute(parameter); }
+        public event EventHandler CanExecuteChanged;
+    }
+}
