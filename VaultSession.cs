1fcd063cdd17a526ba477309c77738d31ecf9536
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-12 20:50:01 +0100

    Add ProgressionContext class
    - Added ProgressionContext class for tracking progression during tasks, class supports CancellationTokens for cancellation, INotifyPropertyChanged for UI updates and IProgress to run progress updates on UI thread

--- VaultSession.cs
+++ VaultSession.cs
@@ -105,6 +105,49 @@ namespace VaultCrypt
             VaultSession.ITERATIONS = iterations;
             VaultSession.KEY = PasswordHelper.DeriveKey(password);
         }
+        public class ProgressionContext : INotifyPropertyChanged
+        {
+            private int _completed;
+            public int Completed
+            {
+                get => _completed;
+                set
+                {
+                    if (_completed == value) return;
+                    _completed = value;
+                    OnPropertyChanged(nameof(Completed));
+                }
+            }
+            private int _total;
+            public int Total
+            {
+                get => _total;
+                set
+                {
+                    if (_total == value) return;
+                    _total = value;
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+            public IProgress<ProgressStatus> Progress { get; init; }
+            public CancellationTokenSource CancellationTokenSource;
+
+
+
+            public CancellationToken CancellationToken => CancellationTokenSource.Token;
+
+            public ProgressionContext()
+            {
+                Completed = 0;
+                Total = 0;
+                CancellationTokenSource = new CancellationTokenSource();
+                Progress = new Progress<ProgressStatus>(p => { Completed = p.completed; Total = p.total; });
+            }
+            private void OnPropertyChanged(string name) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)); }
+            public event PropertyChangedEventHandler? PropertyChanged;
+        }
+
+        internal record ProgressStatus(int completed, int total);
     }
 
 
