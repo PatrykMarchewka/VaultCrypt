a583d594832be583030bf367d92476892e035c2d
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-21 19:08:09 +0100

    Add NavigationService class
    - Added NavigationService class implementing INavigationService

--- Services/NavigationService.cs
+++ Services/NavigationService.cs
@@ -20,6 +20,56 @@ namespace VaultCrypt.Services
         void NavigateFromProgress();
     }
     
+    internal class NavigationService : INavigationService
+    {
+        private void Navigate(IViewModel viewModel, object? parameters = null)
+        {
+            if (viewModel is INavigated nav && parameters != null)
+            {
+                nav.OnNavigatedTo(parameters);
+            }
+            NavigateEvent?.Invoke(viewModel);
+        }
+
+        public void NavigateToMain()
+        {
+            VaultSession.Dispose();
+            Navigate(viewModels.Main);
+        }
+
+        public void NavigateToCreateVault()
+        {
+            Navigate(viewModels.CreateVault);
+        }
+
+        public void NavigateToOpenVault(SecureString password, NormalizedPath vaultPath)
+        {
+            Navigate(viewModels.OpenVault, new { Password = password, VaultPath = vaultPath });
+        }
+
+        public void NavigateToPasswordInput(NormalizedPath vaultPath)
+        {
+            Navigate(viewModels.PasswordInput, vaultPath);
+        }
+
+        public void NavigateToEncryptFile(NormalizedPath filePath)
+        {
+            Navigate(viewModels.EncryptFile, filePath);
+        }
+
+        public void NavigateToProgress(VaultHelper.ProgressionContext context)
+        {
+            Navigate(viewModels.Progress, context);
+        }
+
+        public void NavigateFromProgress()
+        {
+            using var vaultFS = new FileStream(VaultSession.VAULTPATH, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            VaultHelper.RefreshEncryptedFilesList(vaultFS);
+            Navigate(viewModels.OpenVault);
+        }
+    }
+
     interface INavigated
     {
         void OnNavigatedTo(object? parameters);
