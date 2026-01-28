49a985de412df1154701448e1ffe3ee26d773e24
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-28 19:20:43 +0100

    Add ExceptionHandlerService
    - Added ExceptionHandlerService to act as global exception handler and handle exceptions by showcasing information rather than hard crashing to desktop

---
+++ Services/ExceptionHandlerService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Exceptions;
+
+namespace VaultCrypt.Services
+{
+    internal interface IExceptionHandler
+    {
+        void HandleException(Exception ex);
+    }
+
+    internal class ExceptionHandlerService : IExceptionHandler
+    {
+        private readonly IDialogService dialogService;
+        private readonly INavigationService navigationService;
+        public ExceptionHandlerService(IDialogService dialogService, INavigationService navigationService)
+        {
+            this.dialogService = dialogService;
+            this.navigationService = navigationService;
+        }
+
+        public void HandleException(Exception ex)
+        {
+            switch (ex)
+            {
+                case VaultUIException uiEx:
+                    dialogService.ShowErrorWindow(uiEx);
+                    break;
+                case VaultException vEx:
+                    navigationService.NavigateToExceptionThrown(vEx);
+                    break;
+                case OperationCanceledException ocEx:
+                    navigationService.NavigateToExceptionThrown(VaultException.OperationCancelledException(ocEx));
+                    break;
+                default:
+                    navigationService.NavigateToExceptionThrown(ex);
+                    break;
+            }
+        }
+    }
+}
