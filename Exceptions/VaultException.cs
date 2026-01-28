26f314b382048d2f12112a17440107aae9db86c4
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-28 19:22:21 +0100

    Update VaultException.cs
    - Updated VaultException constructor to no longer display inner exception message as it could cause massive chains for deep nested exceptions
    - Added static EndOfFileException for unexpected end of file for streams
    - Added static OperationCancelledException for operations cancelled with CancellationToken

--- Exceptions/VaultException.cs
+++ Exceptions/VaultException.cs
@@ -8,8 +8,12 @@ namespace VaultCrypt.Exceptions
 {
     internal class VaultException : Exception
     {
-        internal VaultException(string message, Exception innerException) : base($"{message} => {innerException.Message}", innerException) { }
+        internal VaultException(string message, Exception? innerException = null) : base(message, innerException) { }
+
+
         internal static VaultException EncryptionFailed(Exception inner) => new VaultException("Encryption failed", inner);
         internal static VaultException DecryptionFailed(Exception inner) => new VaultException("Decryption failed", inner);
+        internal static VaultException EndOfFileException(Exception? inner = null) => new VaultException("Unexpected end of file!", inner);
+        internal static VaultException OperationCancelledException(OperationCanceledException inner) => new VaultException("Operation cancelled by user", inner);
     }
 }
