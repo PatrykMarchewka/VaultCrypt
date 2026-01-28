6528d4528be3563c01a799a51263838616ee16aa
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-28 19:25:23 +0100

    Update startup behaviour
    - Changed startup behaviour to no longer default to MainWindow.xaml
    - Added overriden OnStartup method to prepare viewmodels, services, subscribe to basic events and open the main window

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using VaultCrypt.ViewModels;
+using VaultCrypt.Services;
 
 namespace VaultCrypt
 {
@@ -10,9 +12,28 @@ namespace VaultCrypt
     public partial class App : Application
     {
 
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ViewModelState vms = new ViewModelState();
+            var navigationService = new NavigationService(vms);
+            var dialogService = new DialogService();
+            var exceptionService = new ExceptionHandlerService(dialogService, navigationService);
+            RelayCommand.SubscribeToExceptionThrowEvent((ex) => exceptionService.HandleException(ex));
+            navigationService.ChangeView += viewmodel => vms.MainWindow.CurrentView = viewmodel;
+            navigationService.NavigateToMain();
+            var window = new MainWindow
+            {
+                DataContext = vms.MainWindow
+            };
+            window.Show();
+        }
+
         protected override void OnExit(ExitEventArgs args)
         {
-            VaultSession.Dispose();
+            VaultSession.CurrentSession.Dispose();
             base.OnExit(args);
         }
     }
