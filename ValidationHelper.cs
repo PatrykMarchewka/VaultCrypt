94f717df54996a27e98ca8dab42f1bb96c5d3b52
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-01-28 19:45:44 +0100

    Add ValidationHelper
    - Added ValidationHelper to throw exceptions for UI errors such as empty textbox

---
+++ ValidationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Exceptions;
+
+namespace VaultCrypt
+{
+    internal static class ValidationHelper
+    {
+        internal static void NotEmptyString(string? input, string fieldName)
+        {
+            if (string.IsNullOrWhiteSpace(input)) throw new VaultUIException($"{fieldName} cannot be empty");
+        }
+
+        internal static void NotEmptySecureString(SecureString? input, string fieldName)
+        {
+            if (input is null || input.Length == 0) throw new VaultUIException($"{fieldName} cannot be empty");
+        }
+    }
+}
