b2aacb4bed2169857a6b0bd99465204464424e32
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-04 16:29:41 +0100

    Add AES-CCM support
    - Added AES-CCM 128/192/256 support

--- EncryptionAlgorithm.cs
+++ EncryptionAlgorithm.cs
@@ -14,14 +14,20 @@ namespace VaultCrypt
         {
             AES128GCM,
             AES192GCM,
-            AES256GCM
+            AES256GCM,
+            AES128CCM,
+            AES192CCM,
+            AES256CCM
         }
 
         internal static readonly Dictionary<EncryptionAlgorithmEnum, IEncryptionAlgorithmProvider> GetEncryptionAlgorithmProvider = new()
         {
             {EncryptionAlgorithmEnum.AES128GCM, new Aes128GcmProvider() },
             {EncryptionAlgorithmEnum.AES192GCM, new Aes192GcmProvider() },
-            {EncryptionAlgorithmEnum.AES256GCM, new Aes256GcmProvider() }
+            {EncryptionAlgorithmEnum.AES256GCM, new Aes256GcmProvider() },
+            {EncryptionAlgorithmEnum.AES128CCM, new Aes128CcmProvider() },
+            {EncryptionAlgorithmEnum.AES192CCM, new Aes192CcmProvider() },
+            {EncryptionAlgorithmEnum.AES256CCM, new Aes256CcmProvider() }
         };
 
         internal interface IEncryptionAlgorithm
@@ -122,6 +128,88 @@ namespace VaultCrypt
         {
             public byte KeySize => 32;
             public IEncryptionAlgorithm EncryptionAlgorithm => new AesGcm();
+        }
+
+
+
+        internal class AesCcm : IEncryptionAlgorithm
+        {
+            public short ExtraEncryptionDataSize => 28;
+            public byte[] EncryptBytes(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key)
+            {
+                if (data.Length == 0) throw new VaultException("Failed to encrypt bytes, provided data was empty");
+                if (key.Length == 0) throw new VaultException("Failed to encrypt bytes, provided key was empty");
+
+                byte[] iv = new byte[12];
+                byte[] authentication = new byte[16];
+                byte[] output = new byte[data.Length];
+                byte[] encrypted = new byte[iv.Length + authentication.Length + output.Length];
+                try
+                {
+                    RandomNumberGenerator.Fill(iv);
+                    using (System.Security.Cryptography.AesCcm aesCcm = new System.Security.Cryptography.AesCcm(key))
+                    {
+                        aesCcm.Encrypt(iv, data, output, authentication);
+                    }
+                    Buffer.BlockCopy(iv, 0, encrypted, 0, iv.Length);
+                    Buffer.BlockCopy(authentication, 0, encrypted, iv.Length, authentication.Length);
+                    Buffer.BlockCopy(output, 0, encrypted, iv.Length + authentication.Length, output.Length);
+                    return encrypted;
+                }
+                catch (Exception ex)
+                {
+                    CryptographicOperations.ZeroMemory(encrypted);
+                    throw VaultException.EncryptionFailed(ex);
+                }
+                finally
+                {
+                    CryptographicOperations.ZeroMemory(iv);
+                    CryptographicOperations.ZeroMemory(authentication);
+                    CryptographicOperations.ZeroMemory(output);
+                }
+            }
+
+            public byte[] DecryptBytes(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key)
+            {
+                if (data.Length == 0) throw new VaultException("Failed to decrypt bytes, provided data was empty");
+                if (key.Length == 0) throw new VaultException("Failed to decrypt bytes, provided key was empty");
+
+                ReadOnlySpan<byte> iv = data.Slice(0, 12);
+                ReadOnlySpan<byte> tag = data.Slice(12, 16);
+                ReadOnlySpan<byte> encryptedData = data.Slice(28);
+
+                byte[] decrypted = new byte[encryptedData.Length];
+                try
+                {
+                    using System.Security.Cryptography.AesCcm aesCcm = new System.Security.Cryptography.AesCcm(key);
+                    aesCcm.Decrypt(iv, encryptedData, tag, decrypted);
+                    return decrypted;
+                }
+                catch (Exception ex)
+                {
+                    CryptographicOperations.ZeroMemory(decrypted);
+                    throw VaultException.DecryptionFailed(ex);
+                }
+            }
+        }
+
+        internal class Aes128CcmProvider : IEncryptionAlgorithmProvider
+        {
+            public byte KeySize => 16;
+            public IEncryptionAlgorithm EncryptionAlgorithm => new AesCcm();
+
+        }
+
+        internal class Aes192CcmProvider : IEncryptionAlgorithmProvider
+        {
+            public byte KeySize => 24;
+            public IEncryptionAlgorithm EncryptionAlgorithm => new AesCcm();
+        }
+
+        internal class Aes256CcmProvider : IEncryptionAlgorithmProvider
+        {
+            public byte KeySize => 32;
+            public IEncryptionAlgorithm EncryptionAlgorithm => new AesCcm();
         }
     }
 }
