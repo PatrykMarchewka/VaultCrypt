010ef2bc90b4b900762dd7c6a6f8d7763ba29365
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-06 16:37:04 +0100

    Add threefish algorithm
    - Added support for Threefish algorithm in CTR mode with 256,512 and 1024 bit key/block sizes

--- EncryptionAlgorithm.cs
+++ EncryptionAlgorithm.cs
@@ -27,7 +27,10 @@ namespace VaultCrypt
             AES256EAX,
             Twofish128CTR,
             Twofish192CTR,
-            Twofish256CTR
+            Twofish256CTR,
+            Threefish256CTR,
+            Threefish512CTR,
+            Threefish1024CTR,
         }
 
         internal static readonly Dictionary<EncryptionAlgorithmEnum, IEncryptionAlgorithmProvider> GetEncryptionAlgorithmProvider = new()
@@ -44,7 +47,10 @@ namespace VaultCrypt
             {EncryptionAlgorithmEnum.AES256EAX, new AesProvider(32, new AesEax()) },
             {EncryptionAlgorithmEnum.Twofish128CTR, new TwofishProvider(16, new TwofishCtr()) },
             {EncryptionAlgorithmEnum.Twofish192CTR, new TwofishProvider(24, new TwofishCtr()) },
-            {EncryptionAlgorithmEnum.Twofish256CTR, new TwofishProvider(32, new TwofishCtr()) }
+            {EncryptionAlgorithmEnum.Twofish256CTR, new TwofishProvider(32, new TwofishCtr()) },
+            {EncryptionAlgorithmEnum.Threefish256CTR, new ThreefishProvider(32, new ThreefishCtr(256)) },
+            {EncryptionAlgorithmEnum.Threefish512CTR, new ThreefishProvider(64, new ThreefishCtr(512)) },
+            {EncryptionAlgorithmEnum.Threefish1024CTR, new ThreefishProvider(128, new ThreefishCtr(1024)) }
         };
 
         internal static byte[] CalculateHMAC(ReadOnlySpan<byte> key, params byte[][] bytes)
@@ -75,6 +81,7 @@ namespace VaultCrypt
         private interface AESAlgorithm : IEncryptionAlgorithm;
         private interface ChaCha20Algorithm : IEncryptionAlgorithm;
         private interface TwoFishAlgorithm : IEncryptionAlgorithm;
+        private interface ThreeFishAlgorithm : IEncryptionAlgorithm;
 
 
         internal class AesGcm : AESAlgorithm
@@ -410,6 +417,85 @@ namespace VaultCrypt
             }
         }
 
+        internal class ThreefishCtr : ThreeFishAlgorithm
+        {
+            internal ThreefishCtr(int blockSizeInBits)
+            {
+                if(blockSizeInBits is not 256 and not 512 and not 1024) throw new ArgumentOutOfRangeException(nameof(blockSizeInBits));
+                this.blockSizeInBits = blockSizeInBits;
+            }
+
+            private readonly int blockSizeInBits;
+            public short ExtraEncryptionDataSize => 76;
+
+            public byte[] EncryptBytes(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key)
+            {
+                if (data.Length == 0) throw new VaultException("Failed to encrypt bytes, provided data was empty");
+                if (key.Length == 0) throw new VaultException("Failed to encrypt bytes, provided key was empty");
+
+                byte[] iv = new byte[12];
+                byte[] authentication = new byte[64];
+                byte[] output = new byte[data.Length];
+                byte[] encrypted = new byte[iv.Length + output.Length + authentication.Length];
+                try
+                {
+                    RandomNumberGenerator.Fill(iv);
+                    var cipher = new KCtrBlockCipher(new ThreefishEngine(blockSizeInBits));
+                    var parameters = new ParametersWithIV(new KeyParameter(key), iv);
+                    cipher.Init(true, parameters);
+                    cipher.ProcessBytes(data, output);
+                    authentication = CalculateHMAC(key, iv, output);
+                    Buffer.BlockCopy(iv, 0, encrypted, 0, iv.Length);
+                    Buffer.BlockCopy(output, 0, encrypted, iv.Length, output.Length);
+                    Buffer.BlockCopy(authentication, 0, encrypted, iv.Length + output.Length, authentication.Length);
+                    return encrypted;
+                }
+                catch (Exception ex)
+                {
+                    CryptographicOperations.ZeroMemory(encrypted);
+                    throw VaultException.EncryptionFailed(ex);
+                }
+                finally
+                {
+                    CryptographicOperations.ZeroMemory(iv);
+                    CryptographicOperations.ZeroMemory(authentication);
+                    CryptographicOperations.ZeroMemory(output);
+                }
+            }
+
+            public byte[] DecryptBytes(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key)
+            {
+                if (data.Length == 0) throw new VaultException("Failed to decrypt bytes, provided data was empty");
+                if (key.Length == 0) throw new VaultException("Failed to decrypt bytes, provided key was empty");
+
+                ReadOnlySpan<byte> iv = data.Slice(0, 12);
+                ReadOnlySpan<byte> encryptedData = data[12..^64];
+                ReadOnlySpan<byte> tag = data[^64..];
+
+                byte[] decrypted = new byte[encryptedData.Length];
+                byte[] calculatedTag = new byte[64];
+                try
+                {
+                    calculatedTag = CalculateHMAC(key, iv.ToArray(), encryptedData.ToArray());
+                    if (!CryptographicOperations.FixedTimeEquals(tag, calculatedTag)) throw new VaultException("Wrong HMAC authentication tag");
+                    var cipher = new KCtrBlockCipher(new ThreefishEngine(blockSizeInBits));
+                    var parameters = new ParametersWithIV(new KeyParameter(key), iv);
+                    cipher.Init(false, parameters);
+                    cipher.ProcessBytes(encryptedData, decrypted);
+                    return decrypted;
+                }
+                catch (Exception ex)
+                {
+                    CryptographicOperations.ZeroMemory(decrypted);
+                    throw VaultException.DecryptionFailed(ex);
+                }
+                finally
+                {
+                    CryptographicOperations.ZeroMemory(calculatedTag);
+                }
+            }
+        }
+
 
 
         internal interface IEncryptionAlgorithmProvider
@@ -463,6 +549,23 @@ namespace VaultCrypt
                 ArgumentNullException.ThrowIfNull(keySize);
                 ArgumentNullException.ThrowIfNull(algorithm);
                 if (keySize is not 16 and not 24 and not 32) throw new ArgumentOutOfRangeException(nameof(keySize));
+
+                KeySize = keySize;
+                EncryptionAlgorithm = algorithm;
+            }
+        }
+
+        private class ThreefishProvider : IEncryptionAlgorithmProvider
+        {
+            public byte KeySize { get; }
+
+            public IEncryptionAlgorithm EncryptionAlgorithm { get; }
+
+            internal ThreefishProvider(byte keySize, ThreeFishAlgorithm algorithm)
+            {
+                ArgumentNullException.ThrowIfNull(keySize);
+                ArgumentNullException.ThrowIfNull(algorithm);
+                if (keySize is not 32 and not 64 and not 128) throw new ArgumentOutOfRangeException(nameof(keySize));
 
                 KeySize = keySize;
                 EncryptionAlgorithm = algorithm;
