72462d28400dd1fbf6a56437e8f9e99020a61538
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-12 15:36:18 +0100

    Remove old exception constructors
    - Removed old and now unused constructor and static factory methods

--- Exceptions/VaultException.cs
+++ Exceptions/VaultException.cs
@@ -8,14 +8,6 @@ namespace VaultCrypt.Exceptions
 {
     internal class VaultException : Exception
     {
-        internal VaultException(string message, Exception? innerException = null) : base(message, innerException) { }
-
-
-        internal static VaultException EncryptionFailed(Exception inner) => new VaultException("Encryption failed", inner);
-        internal static VaultException DecryptionFailed(Exception inner) => new VaultException("Decryption failed", inner);
-        internal static VaultException EndOfFileException(Exception? inner = null) => new VaultException("Unexpected end of file!", inner);
-        internal static VaultException OperationCancelledException(OperationCanceledException inner) => new VaultException("Operation cancelled by user", inner);
-
         internal ErrorContext ExceptionContext { get; }
         internal ErrorReason ExceptionReason { get; }
         
