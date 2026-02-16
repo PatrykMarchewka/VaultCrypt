52350c9d15aca8b1ba1d05f439a5c31d0be0b35d
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-16 18:08:51 +0100

    Add VaultService mock
    - Created mock of VaultService for testing

---
+++ VaultCrypt.Tests/Services/FakeVaultService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Services;
+
+namespace VaultCrypt.Tests.Services
+{
+    internal class FakeVaultService : IVaultService
+    {
+        public bool CreateVaultWasCalled = false;
+        public void CreateVault(NormalizedPath folderPath, string vaultName, byte[] password, int iterations) => CreateVaultWasCalled = true;
+    }
+}
