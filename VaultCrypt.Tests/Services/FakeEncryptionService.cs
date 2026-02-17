65a51261909038a3e4aca59195cf25f75bcdd95f
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-17 16:47:29 +0100

    Add EncryptionService mock
    - Added EncryptionService mock for easy testing

---
+++ VaultCrypt.Tests/Services/FakeEncryptionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Services;
+
+namespace VaultCrypt.Tests.Services
+{
+    internal class FakeEncryptionService : IEncryptionService
+    {
+        public bool EncryptWasCalled = false;
+        public Task Encrypt(EncryptionAlgorithm.EncryptionAlgorithmInfo algorithm, ushort chunkSizeInMB, NormalizedPath filePath, ProgressionContext context)
+        {
+            EncryptWasCalled = true;
+            return Task.CompletedTask;
+        }
+    }
+}
