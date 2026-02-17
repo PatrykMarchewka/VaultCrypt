29e7ff8af24153a828dc5ab22bccb31aa7761448
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-17 18:23:57 +0100

    Add MainWindowViewModel tests
    - Added basic tests to check MainWindowViewModel
    - Changed visibility of viewmodel to public in order to allow testing

---
+++ VaultCrypt.Tests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.ViewModels;
+
+namespace VaultCrypt.Tests.ViewModels
+{
+    public class MainWindowViewModelTests
+    {
+        private VaultCrypt.ViewModels.MainWindowViewModel _viewModel;
+
+        public MainWindowViewModelTests()
+        {
+            this._viewModel = new VaultCrypt.ViewModels.MainWindowViewModel();
+        }
+
+        [Fact]
+        void CurrentViewRaisesPropertyChanged()
+        {
+            string? changedProperty = null;
+            _viewModel.PropertyChanged += (sender, args) => { changedProperty = args.PropertyName; };
+
+            _viewModel.CurrentView = new FakeViewModel();
+
+            Assert.Equal(nameof(_viewModel.CurrentView), changedProperty);
+        }
+
+        [Fact]
+        void CurrentViewDoesNotRaisePropertyChanged()
+        {
+            IViewModel fake = new FakeViewModel();
+            _viewModel.CurrentView = fake;
+            int eventRaisedCount = 0;
+            _viewModel.PropertyChanged += (sender, args) => { eventRaisedCount++; };
+            _viewModel.CurrentView = fake;
+
+            Assert.Equal(0, eventRaisedCount);
+        }
+
+        [Fact]
+        void CurrentViewChangesValue()
+        {
+            IViewModel expected = new FakeViewModel();
+            _viewModel.CurrentView = expected;
+
+            Assert.Equal(expected, _viewModel.CurrentView);
+        }
+
+    }
+}
