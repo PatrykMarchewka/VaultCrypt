ecba96dc173977069161ffb14fa79c58e0532b1a
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-19 19:01:44 +0100

    Move NormalizedPath out of FileHelper
    - Moved NormalizedPath class out of FileHelper in order to decouple

--- FileHelper.cs
+++ FileHelper.cs
@@ -239,37 +239,4 @@ namespace VaultCrypt
         }
 
     }
-    internal class NormalizedPath
-    {
-        internal string Value { get; }
-        private NormalizedPath(string path)
-        {
-            Value = Normalize(path);
-        }
-        private static string Normalize(string path)
-        {
-            return path.Length > 260 && !path.StartsWith(@"\\?\") ? @"\\?\" + path : path;
-        }
-
-        /// <summary>
-        /// Normalizes string and returns it as NormalizedPath
-        /// </summary>
-        /// <param name="input">String to normalize</param>
-        /// <returns>NormalizedPath containing the path from the input</returns>
-        internal static NormalizedPath? From(string? input)
-        {
-            if (input is null) return null;
-            return new NormalizedPath(input);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
-
-        public static implicit operator string?(NormalizedPath? path) => path?.Value;
-
-
-    }
-
 }
---
+++ NormalizedPath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultCrypt
+{
+    public class NormalizedPath
+    {
+        public string Value { get; }
+        private NormalizedPath(string path)
+        {
+            Value = Normalize(path);
+        }
+        private static string Normalize(string path)
+        {
+            return path.Length > 260 && !path.StartsWith(@"\\?\") ? @"\\?\" + path : path;
+        }
+
+        /// <summary>
+        /// Normalizes string and returns it as NormalizedPath
+        /// </summary>
+        /// <param name="input">String to normalize</param>
+        /// <returns>NormalizedPath containing the path from the input</returns>
+        public static NormalizedPath? From(string? input)
+        {
+            if (input is null) return null;
+            return new NormalizedPath(input);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public static implicit operator string?(NormalizedPath? path) => path?.Value;
+
+
+    }
+}
