59737ed4f7c4229c6e939174bddfc98f565498c8
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-24 20:24:42 +0100

    Add fake file service
    - Added fake file service for testing purposes

---
+++ VaultCrypt.Tests/Services/FakeFileService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Services;
+
+namespace VaultCrypt.Tests.Services
+{
+    public class FakeFileService : IFileService
+    {
+        public bool CopyPartOfFileWasCalled = false;
+        public bool WriteReadyChunkWasCalled = false;
+        public bool ZeroOutPartOfFileWasCalled = false;
+        public void CopyPartOfFile(Stream source, long offset, ulong length, Stream destination, long destinationOffset) => CopyPartOfFileWasCalled = true;
+
+        public void WriteReadyChunk(ConcurrentDictionary<int, byte[]> results, ref int nextToWrite, int currentIndex, Stream fileFS, object lockObject) => WriteReadyChunkWasCalled = true;
+
+        public void ZeroOutPartOfFile(Stream stream, long offset, ulong length) => ZeroOutPartOfFileWasCalled = true;
+    }
+}
