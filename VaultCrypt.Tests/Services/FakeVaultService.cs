4f399362c0f15c395720275b73cc09eaa277a77f
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-02-24 17:34:01 +0100

    Update FakeVaultService
    - Updated FakeVaultService after merge to implement all methods from implemented interface

--- VaultCrypt.Tests/Services/FakeVaultService.cs
+++ VaultCrypt.Tests/Services/FakeVaultService.cs
@@ -9,7 +9,20 @@ namespace VaultCrypt.Tests.Services
 {
     internal class FakeVaultService : IVaultService
     {
+        public bool CreateSessionFromFileWasCalled = false;
         public bool CreateVaultWasCalled = false;
+        public bool DeleteFileFromVaultWasCalled = false;
+        public bool RefreshEncryptedFilesListWasCalled = false;
+        public bool TrimVaultWasCalled = false;
+
+        public void CreateSessionFromFile(byte[] password, NormalizedPath path) => CreateSessionFromFileWasCalled = true;
+
         public void CreateVault(NormalizedPath folderPath, string vaultName, byte[] password, int iterations) => CreateVaultWasCalled = true;
+
+        public void DeleteFileFromVault(KeyValuePair<long, EncryptedFileInfo> FileMetadataEntry, ProgressionContext context) => DeleteFileFromVaultWasCalled = true;
+
+        public void RefreshEncryptedFilesList(Stream vaultFS) => RefreshEncryptedFilesListWasCalled = true;
+
+        public void TrimVault(ProgressionContext context) => TrimVaultWasCalled = true;
     }
 }
