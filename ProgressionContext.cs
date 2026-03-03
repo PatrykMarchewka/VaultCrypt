6bf00b71580038d0005d96ff36ac58d8390c7c3b
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-03-03 18:31:50 +0100

    Update ProgressionContext
    - ProgressionContext starts with value of Total field set to 1 to show 0/1 instead of 0/0 when no reports have been done yet
    - Changed the ProgressStatus record to throw for invalid values
    - Updated viewmodel to remove redundant checks and improve readability

--- ProgressionContext.cs
+++ ProgressionContext.cs
@@ -41,12 +41,26 @@ namespace VaultCrypt
         public ProgressionContext()
         {
             Completed = 0;
-            Total = 0;
+            Total = 1; //Starting at 1 so user sees 0/1 instead 0/0
             CancellationTokenSource = new CancellationTokenSource();
-            Progress = new Progress<ProgressStatus>(p => { Completed = p.completed; Total = p.total; });
+            Progress = new Progress<ProgressStatus>(p => { Completed = p.Completed; Total = p.Total; });
         }
         private void OnPropertyChanged(string name) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)); }
         public event PropertyChangedEventHandler? PropertyChanged;
     }
-    public record ProgressStatus(int completed, int total);
+    public record ProgressStatus
+    {
+        public int Completed { get; }
+        public int Total { get; }
+
+        public ProgressStatus(int completed, int total)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(completed);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(total);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(completed, total);
+
+            Completed = completed;
+            Total = total;
+        }
+    }
 }
