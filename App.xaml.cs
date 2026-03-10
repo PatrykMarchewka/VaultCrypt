d32e4bb90eb462764968e3fc59d22f90aca7584e
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-03-10 17:52:04 +0100

    Change viewmodels to static for clarity
    - Changed viewmodels and viewmodel list in ViewModelState to static to better signal clarity and intent
    
    - Decoupled ViewModelState from NavigationService
    Since viewmodels in ViewModelState are static they can be easily referenced without injecting it into the service

--- App.xaml.cs
+++ App.xaml.cs
@@ -17,17 +17,16 @@ namespace VaultCrypt
         {
             base.OnStartup(e);
 
-            ViewModelState vms = new ViewModelState();
-            var navigationService = new NavigationService(vms, VaultSession.CurrentSession);
+            var navigationService = new NavigationService(VaultSession.CurrentSession);
             var dialogService = new DialogService();
             var exceptionService = new ExceptionHandlerService(dialogService, navigationService);
             RelayCommand.SubscribeToExceptionThrowEvent((ex) => exceptionService.HandleException(ex));
-            navigationService.ChangeView += viewmodel => vms.MainWindow.CurrentView = viewmodel;
+            navigationService.ChangeView += viewmodel => ViewModelState.MainWindow.CurrentView = viewmodel;
             if (e.Args.Length > 0) navigationService.NavigateToPasswordInput(NormalizedPath.From(e.Args[0]));
             else navigationService.NavigateToMain();
             var window = new MainWindow
             {
-                DataContext = vms.MainWindow
+                DataContext = ViewModelState.MainWindow
             };
             window.Show();
         }
