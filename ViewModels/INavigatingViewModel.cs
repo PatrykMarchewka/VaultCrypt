3f87d3f3b352305c95e140a929942019d015fda2
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-04-10 13:14:17 +0200

    Add INavigatingViewModel documentation
    - Added XML documentation to INavigatingViewmodel

--- ViewModels/INavigatingViewModel.cs
+++ ViewModels/INavigatingViewModel.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace VaultCrypt.ViewModels
 {
+    /// <summary>
+    /// Represents viewmodel that can be navigated to and/or navigates to other viewmodels
+    /// </summary>
     public interface INavigatingViewModel
     {
+        /// <summary>
+        /// Event used to request navigation to another viewmodel
+        /// </summary>
         public event Action<NavigationRequest> NavigationRequested;
     }
 }
