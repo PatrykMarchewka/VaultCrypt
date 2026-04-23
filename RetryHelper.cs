dc7df375b441b93ec99b1c5784ff3bb2863925ee
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-04-23 15:59:15 +0200

    Add RetryHelper class
    - Added RetryHelper class focused on holding methods that allow retrying methods even if they throw

---
+++ RetryHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultCrypt
+{
+    public sealed class RetryHelper
+    {
+        public static async Task<T> TryUntilSuccess<T>(Func<Task<T>> tryAction, Func<Task>? catchAction = null, int maxRetries = 100)
+        {
+            for (int attempt = 1; attempt <= maxRetries; attempt++)
+            {
+                try
+                {
+                    return await tryAction();
+                }
+                catch (Exception ex)
+                {
+                    if (attempt == maxRetries) throw new VaultCrypt.Exceptions.VaultException(Exceptions.VaultException.ErrorContext.VaultSession, Exceptions.VaultException.ErrorReason.MaxRetriesReached, ex);
+                    if(catchAction is not null) await catchAction();
+                    await Task.Delay(1000);
+                }
+            }
+            throw new UnreachableException(); //Should never reach this point
+        }
+
+        public static async Task<T> TryUntilSuccess<T>(Func<Task<T>> tryAction, Action? catchAction = null, int maxRetries = 100)
+        {
+            return await TryUntilSuccess<T>(tryAction, catchAction: catchAction is null ? null : () => { catchAction(); return Task.CompletedTask; }, maxRetries);
+        }
+
+        public static void TryUntilSuccess(Action tryAction, Action? catchAction = null, int maxRetries = 100)
+        {
+            TryUntilSuccess<bool>(
+                tryAction: () => { tryAction(); return Task.FromResult(true); },
+                catchAction: catchAction is null ? null : () => { catchAction(); return Task.CompletedTask; },
+                maxRetries
+                ).GetAwaiter().GetResult();
+        }
+    }
+}
