da05848dd6076e0319a648ab4b05f8b59395b430
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-04-23 20:49:27 +0200

    Update ProgressionContext tests
    - Updated ProgressionContext tests to match recent changes

--- VaultCrypt.Tests/ProgressionContextTests.cs
+++ VaultCrypt.Tests/ProgressionContextTests.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using VaultCrypt.ViewModels;
 
 namespace VaultCrypt.Tests
 {
@@ -22,20 +22,20 @@ namespace VaultCrypt.Tests
         {
             Assert.Equal(0UL, _progressionContext.Completed);
             Assert.Equal(1UL, _progressionContext.Total);
+            Assert.Null(_progressionContext.Progress);
             Assert.False(_progressionContext.CancellationToken.Equals(default));
         }
 
         [Fact]
         void IncrementIncreasesCompletedValue()
         {
+            _progressionContext.SetTotal(ulong.MaxValue);
             _progressionContext.Increment(ulong.MaxValue);
             Assert.Equal(ulong.MaxValue, _progressionContext.Completed);
             _progressionContext.Increment();
             Assert.Equal(0UL, _progressionContext.Completed);
             _progressionContext.Increment(2);
             Assert.Equal(2UL, _progressionContext.Completed);
-            
-
         }
 
         [Fact]
@@ -45,7 +45,7 @@ namespace VaultCrypt.Tests
             var eventBlock = new BlockingCollection<object>();
 
             int progressReported = 0;
-            var progress = new Progress<ProgressionContext>(_ => { progressReported++; eventBlock.Add(new object()); }); 
+            var progress = new Progress<ProgressReported>(_ => { progressReported++; eventBlock.Add(new object()); }); 
             _progressionContext.Progress = progress;
 
             _progressionContext.Increment();
@@ -60,14 +60,19 @@ namespace VaultCrypt.Tests
         }
 
         [Fact]
-        void SetTotalChangesValue()
+        void IncrementThrowsOnCompletedGreaterThanTotal()
         {
-            _progressionContext.SetTotal(1);
-            Assert.Equal(1UL, _progressionContext.Total);
-            _progressionContext.SetTotal(2);
-            Assert.Equal(2UL, _progressionContext.Total);
-            _progressionContext.SetTotal(ulong.MaxValue);
-            Assert.Equal(ulong.MaxValue, _progressionContext.Total);
+            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.Increment(int.MaxValue));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(ulong.MaxValue)]
+        void SetTotalChangesValue(ulong total)
+        {
+            _progressionContext.SetTotal(total);
+            Assert.Equal(total, _progressionContext.Total);
         }
 
         [Fact]
@@ -77,7 +82,7 @@ namespace VaultCrypt.Tests
             var eventBlock = new BlockingCollection<object>();
 
             int progressReported = 0;
-            var progress = new Progress<ProgressionContext>(_ => { progressReported++; eventBlock.Add(new object()); });
+            var progress = new Progress<ProgressReported>(_ => { progressReported++; eventBlock.Add(new object()); });
             _progressionContext.Progress = progress;
 
             _progressionContext.SetTotal(1);
@@ -86,17 +91,120 @@ namespace VaultCrypt.Tests
         }
 
         [Fact]
-        void SetTotalThrowsOnZeroValue()
+        void SetTotalThrowsOnTotalLessThanCompleted()
+        {
+            int total = 100;
+            _progressionContext.SetTotal(total);
+            _progressionContext.Increment((ulong)total);
+            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(total - 1));
+        }
+
+        [Fact]
+        void SetTotalThrowsOnZeroTotal()
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(0));
         }
 
-        [Fact]
-        void SetTotalThrowsOnNegativeValues()
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-2)]
+        [InlineData(int.MinValue)]
+        void SetTotalThrowsOnNegativeTotal(int total)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(-1));
-            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(-2));
-            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(int.MinValue));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.SetTotal(total));
+        }
+
+        [Fact]
+        void ForceFinishSetsCompletedToTotal()
+        {
+            ulong value = 1000;
+            _progressionContext.SetTotal(value);
+
+            _progressionContext.ForceFinish();
+
+            Assert.Equal(value, _progressionContext.Completed);
+            Assert.Equal(value, _progressionContext.Total);
+        }
+
+        [Fact]
+        void ForceFinishCallsProgressReport()
+        {
+            //Event block to either return given item or wait desired time
+            var eventBlock = new BlockingCollection<object>();
+
+            int progressReported = 0;
+            var progress = new Progress<ProgressReported>(_ => { progressReported++; eventBlock.Add(new object()); });
+            _progressionContext.Progress = progress;
+
+            _progressionContext.ForceFinish();
+            eventBlock.TryTake(out _, 1000);
+            Assert.Equal(1, progressReported);
+        }
+
+        [Fact]
+        void ReportPermStatusCallsProgressReport()
+        {
+            //Event block to either return given item or wait desired time
+            var eventBlock = new BlockingCollection<object>();
+
+            int progressReported = 0;
+            var progress = new Progress<ProgressReported>(_ => { progressReported++; eventBlock.Add(new object()); });
+            _progressionContext.Progress = progress;
+
+            _progressionContext.ReportPermStatus((ProgressFailure.ProgressPermFailure)1, "message");
+            eventBlock.TryTake(out _, 1000);
+            Assert.Equal(1, progressReported);
+        }
+
+        [Fact]
+        void ReportPermStatusThrowsOnNullMessage()
+        {
+            Assert.Throws<ArgumentNullException>(() => _progressionContext.ReportPermStatus((ProgressFailure.ProgressPermFailure)1, null!));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        void ReportPermStatusThrowsOnInvalidMessage(string message)
+        {
+            Assert.Throws<ArgumentException>(() => _progressionContext.ReportPermStatus((ProgressFailure.ProgressPermFailure)1, message));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(int.MinValue)]
+        void ReportPermStatusThrowsOnNegativeNumber(int number)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _progressionContext.ReportPermStatus((ProgressFailure.ProgressPermFailure)1, number));
+        }
+
+        [Theory]
+        [InlineData(default(ProgressFailure.ProgressPermFailure))]
+        void ReportPermStatusThrowsOnInvalidFailure(ProgressFailure.ProgressPermFailure failure)
+        {
+            Assert.Throws<ArgumentException>(() => _progressionContext.ReportPermStatus(failure, "message"));
+        }
+
+        [Fact]
+        void ReportTempStatusCallsProgressReport()
+        {
+            //Event block to either return given item or wait desired time
+            var eventBlock = new BlockingCollection<object>();
+
+            int progressReported = 0;
+            var progress = new Progress<ProgressReported>(_ => { progressReported++; eventBlock.Add(new object()); });
+            _progressionContext.Progress = progress;
+
+            _progressionContext.ReportTempStatus((ProgressFailure.ProgressTempFailure)1);
+            eventBlock.TryTake(out _, 1000);
+            Assert.Equal(1, progressReported);
+        }
+
+        [Theory]
+        [InlineData(default(ProgressFailure.ProgressTempFailure))]
+        void ReportTempStatusThrowsOnInvalidFailure(ProgressFailure.ProgressTempFailure failure)
+        {
+            Assert.Throws<ArgumentException>(() => _progressionContext.ReportTempStatus(failure));
         }
 
         [Fact]
