37857c5c5fa629341c5b03887da45731893ed20c
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-05-20 18:28:08 +0200

    Add fallback if provided fieldName is empty
    - Add fallback value if provided fieldName is empty

--- ValidationHelper.cs
+++ ValidationHelper.cs
@@ -12,11 +12,13 @@ namespace VaultCrypt
     {
         public static void NotEmptyString(string? input, string fieldName)
         {
+            if (string.IsNullOrWhiteSpace(fieldName)) fieldName = "[Unknown field]";
             if (string.IsNullOrWhiteSpace(input)) throw new VaultUIException($"{fieldName} cannot be empty");
         }
 
         public static void NotEmptySecureString(SecureString? input, string fieldName)
         {
+            if (string.IsNullOrWhiteSpace(fieldName)) fieldName = "[Unknown field]";
             if (input is null || input.Length == 0) throw new VaultUIException($"{fieldName} cannot be empty");
         }
     }
