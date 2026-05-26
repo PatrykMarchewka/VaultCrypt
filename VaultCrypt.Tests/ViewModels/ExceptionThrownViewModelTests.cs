ff7c9d12f0073625932d4bb3fed01cca764900aa
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-05-26 20:28:15 +0200

    Remove constructor and simplify field creation
    - Removed constructor and simplified field creation

--- VaultCrypt.Tests/ViewModels/ExceptionThrownViewModelTests.cs
+++ VaultCrypt.Tests/ViewModels/ExceptionThrownViewModelTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,7 @@ namespace VaultCrypt.Tests.ViewModels
 {
     public class ExceptionThrownViewModelTests
     {
-        private VaultCrypt.ViewModels.ExceptionThrownViewModel _viewModel;
-
-        public ExceptionThrownViewModelTests()
-        {
-            this._viewModel = new();
-        }
+        private readonly VaultCrypt.ViewModels.ExceptionThrownViewModel _viewModel = new();
 
         [Fact]
         internal void ExceptionMessageRaisesPropertyChanged()
