6e6d0260989cef955f2a1469dcb5a977c3bfe093
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-03 23:32:21 +0200

    Add method to validate NormalizedPath
    - Added methods to validate NormalizedPath and whether it points to already existing file

--- NormalizedPath.cs
+++ NormalizedPath.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,27 @@ namespace VaultCrypt
         {
             Value = Normalize(path);
         }
+
+        /// <summary>
+        /// Validates <paramref name="filePath"/> by checking whether it points to a valid location, optionally checking if file exists at that location
+        /// </summary>
+        /// <param name="filePath">Path to check</param>
+        /// <param name="ensureExists">Dictates whether to ensure that file exists at the provided <paramref name="filePath"/></param>
+        /// <returns>True if every check is satisfied, otherwise false</returns>
+        public static bool ValidatePath(NormalizedPath filePath, bool ensureExists = false)
+        {
+            if (!Path.IsPathRooted(filePath)) return false;
+            if (ensureExists && !File.Exists(filePath)) return false;
+
+            return true;
+        }
+
+        /// <inheritdoc cref="ValidatePath(NormalizedPath, bool)"/>
+        public static bool ValidatePath(string filePath, bool ensureExists = false)
+        {
+            return ValidatePath(From(filePath), ensureExists);
+        }
+
         private static string Normalize(string path)
         {
             return path.Length > 260 && !path.StartsWith(@"\\?\") ? @"\\?\" + path : path;
