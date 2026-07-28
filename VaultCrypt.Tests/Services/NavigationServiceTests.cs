fa9dcd6e2c3ee8f160ba598de5517cc2e014d105
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-28 21:21:09 +0200

    Add tests for NavigationService
    - Added tests for NavigationService

---
+++ VaultCrypt.Tests/Services/NavigationServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultCrypt.Services;
+
+namespace VaultCrypt.Tests.Services
+{
+    public class NavigationServiceTests
+    {
+        private readonly NavigationService _navigationService = new();
+
+        [Fact]
+        internal void HandleNavigationThrowsForInvalidValues()
+        {
+            Assert.Throws<ArgumentNullException>(() => _navigationService.HandleNavigation(null!));
+        }
+
+        [Fact]
+        internal void NavigateToMainDisposesProperly()
+        {
+            //Setting current sesion to ensure fields are not default
+            VaultSession.CurrentSession = TestsHelper.EmptyVaultV0Information.VaultSession;
+            Assert.NotEqual(string.Empty, VaultSession.CurrentSession.VAULTPATH);
+
+            _navigationService.NavigateToMain();
+
+            //Asserting that dispose sets vault path field to default
+            Assert.Equal(string.Empty, VaultSession.CurrentSession.VAULTPATH);
+        }
+
+        public static TheoryData<ISecureBuffer, Type> InvalidBuffers = new TheoryData<ISecureBuffer, Type>()
+        {
+            {null!, typeof(ArgumentNullException) },
+            {TestsHelper.EmptySecureBuffer, typeof(ArgumentException) }
+        };
+
+        [Theory]
+        [MemberData(nameof(InvalidBuffers))]
+        internal void NavigateToOpenVaultThrowsForInvalidPassword(ISecureBuffer password, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => _navigationService.NavigateToOpenVault(password, NormalizedPath.From("TEST")));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestsHelper.InvalidPaths), MemberType = typeof(TestsHelper))]
+        internal void NavigateToOpenVaultThrowsForInvalidVaultPath(NormalizedPath vaultPath, Type expectedException)
+        {
+            using (ISecureBuffer _ = SecureBuffer.Create(1))
+            {
+                Assert.Throws(expectedException, () => _navigationService.NavigateToOpenVault(_, vaultPath));
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestsHelper.InvalidPaths), MemberType = typeof(TestsHelper))]
+        internal void NavigateToPasswordInputThrowsForInvalidValues(NormalizedPath vaultPath, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => _navigationService.NavigateToPasswordInput(vaultPath));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestsHelper.InvalidPaths), MemberType = typeof(TestsHelper))]
+        internal void NavigateToEncryptFileThrowsForInvalidValues(NormalizedPath filePath, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => _navigationService.NavigateToEncryptFile(filePath));
+        }
+
+        [Fact]
+        internal void NavigateToProgressThrowsForInvalidValues()
+        {
+            Assert.Throws<ArgumentNullException>(() => _navigationService.NavigateToProgress(null!));
+        }
+
+        [Fact]
+        internal void NavigateToExceptionThrownThrowsForInvalidValues()
+        {
+            Assert.Throws<ArgumentNullException>(() => _navigationService.NavigateToExceptionThrown(null!));
+        }
+    }
+}
