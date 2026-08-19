c114c8cfb9c44a52dba91a2d2538ea59caa0b4f7
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-08-19 14:32:23 +0200

    Change IFileDialogService to use Task in methods
    - Changed IFileDialogService to use Task in methods
    This change was made to accomodate for future Avalonia implementation since it exposes only async implementation

--- VaultCrypt.Core/Services/IFileDialogService.cs
+++ VaultCrypt.Core/Services/IFileDialogService.cs
@@ -8,17 +8,17 @@ public interface IFileDialogService
     /// <param name="title">Title of the window</param>
     /// <param name="allFiles">Indicates whether to show all files or just vault files</param>
     /// <returns>String containing the full filepath of selected file</returns>
-    public string? OpenFile(string title, bool allFiles);
+    public Task<string?> OpenFile(string title, bool allFiles);
     /// <summary>
     /// Opens folder dialog window
     /// </summary>
     /// <param name="title">Title of the window</param>
     /// <returns>String containing the full path to the selected folder</returns>
-    public string? OpenFolder(string title);
+    public Task<string?> OpenFolder(string title);
     /// <summary>
     /// Opens save file dialog window
     /// </summary>
     /// <param name="fileName">Original name of file to save</param>
     /// <returns>String containing the full filepath</returns>
-    public string? SaveFile(string fileName);
+    public Task<string?> SaveFile(string fileName);
 }
