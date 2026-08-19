a2c4d71ab389deaa608c2d0c9c8e2d5b28a4889b
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-08-19 15:03:45 +0200

    Move DialogWindow and MainWindow to WPF
    - Moved DialogWindow and MainWindow to WPF
    - Removed Width and Height properties from DialogWindow to avoid confusion about window size as it should always be using SizeToContent method

---
+++ VaultCrypt.WPF/DialogWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace VaultCrypt.WPF;
+
+public partial class DialogWindow : Window
+{
+    public DialogWindow()
+    {
+        InitializeComponent();
+    }
+}
--- VaultCrypt.WPF/MainWindow.xaml.cs
+++ VaultCrypt.WPF/MainWindow.xaml.cs
@@ -8,18 +8,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using VaultCrypt.ViewModels;
 
-namespace VaultCrypt
+namespace VaultCrypt.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
