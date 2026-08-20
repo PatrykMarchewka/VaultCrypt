4c058f0dc3480e7956fe5d03a540038e9ce1eec7
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-08-20 22:41:50 +0200

    Add CollectionManager tests
    - Added tests ensuring CollectionManager works properly

---
+++ VaultCrypt.Tests/ViewModels/CollectionManagerTests.cs
@@ -0,0 +1,72 @@
+﻿using VaultCrypt.ViewModels;
+
+namespace VaultCrypt.Tests.ViewModels;
+
+public class CollectionManagerTests
+{
+    private readonly List<int> _testData = new List<int>() { 1, 2, 3, 4, 5 };
+
+    [Fact]
+    internal void ItemsReturnsCorrectForUnsortedUnfiltered()
+    {
+        var manager = new CollectionManager<int>(_testData);
+        
+        Assert.Equal(_testData, manager.Items);
+    }
+
+    [Fact]
+    internal void ItemsReturnsCorrectForSortedUnfiltered()
+    {
+        var expected = _testData;
+        var manager = new CollectionManager<int>(_testData);
+
+        //Reverses the order
+        manager.Sort = (first, second) => second.CompareTo(first);
+
+        expected.Reverse();
+        Assert.Equal(expected, manager.Items);
+    }
+
+    [Fact]
+    internal void ItemsReturnsCorrectForSortedAndFiltered()
+    {
+        var expected = _testData;
+        var manager = new CollectionManager<int>(_testData);
+        
+        //Reverses the order
+        manager.Sort = (first, second) => second.CompareTo(first);
+        manager.Filter = item => item % 2 == 0;
+
+        expected.Reverse();
+        
+        Assert.Equal(expected.Where(item => item % 2 == 0), manager.Items);
+    }
+
+    [Fact]
+    internal void RefreshRaisesPropertyChanged()
+    {
+        var manager = new CollectionManager<int>(_testData);
+        int propertyRaisedCount = 0;
+        manager.PropertyChanged += (sender, args) => propertyRaisedCount++;
+        
+        manager.Refresh();
+        
+        Assert.Equal(1, propertyRaisedCount);
+    }
+
+    [Fact]
+    internal void RefreshRaisesPropertyChangedOnEachCall()
+    {
+        var manager = new CollectionManager<int>(_testData);
+        int propertyRaisedCount = 0;
+        manager.PropertyChanged += (sender, args) => propertyRaisedCount++;
+        
+        manager.Refresh();
+        manager.Refresh();
+        manager.Refresh();
+        manager.Refresh();
+        manager.Refresh();
+        
+        Assert.Equal(5, propertyRaisedCount);
+    }
+}
